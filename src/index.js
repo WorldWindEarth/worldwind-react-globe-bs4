410b5dd8bf6481778e912ca0132fc064616fb47f
Bruce Schubert <bruce@emxsys.com>
2018-05-31 06:31:34 -0700

    Added NavBar and NavItem components.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import LayerButton from './components/LayerButton';
 import LayerList from './components/LayerList';
 import Layers from './components/Layers';
 import Markers from './components/Markers';
+import NavBar from './components/NavBar';
+import NavItem from './components/NavItem';
 import Settings from './components/Settings';
 import SearchBox from './components/SearchBox';
 import SearchPreview from './components/SearchPreview';
@@ -17,6 +19,8 @@ export {
   LayerList,
   LayerButton,
   Markers,
+  NavBar,
+  NavItem,
   Settings,
   SearchBox,
   SearchPreview,
