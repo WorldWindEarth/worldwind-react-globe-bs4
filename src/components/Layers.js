99309b3358b8822e20c6b1e6f60cbe05c7a13677
Bruce Schubert <bruce@emxsys.com>
2018-05-30 20:32:33 -0700

    Updated Settings to process layer lists.

--- src/components/Layers.js
+++ src/components/Layers.js
@@ -1,36 +1,36 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Globe from 'worldwind-react-globe';
-
-import LayerList from './LayerList';
+import { LayerList } from 'worldwind-react-globe-bs4';
 import './Layers.css';
 
 export default class Layers extends Component {
 
-    static propTypes = {
-        baseLayers: PropTypes.object.isRequired,
-        overlayLayers: PropTypes.object.isRequired,
-        globe: PropTypes.instanceOf(Globe)
-    }   
+  static propTypes = {
+      layerLists: PropTypes.array.isRequired,
+      globe: PropTypes.instanceOf(Globe)
+  }   
 
-    render() {
-        // Create a Bootstrap card that renders the base and overlay layer lists
-        return (
-            <div className="card globe-card w-100">
-                <div className="card-header">
-                    <h5 className="card-title">
-                        <span className="fas fa-list" aria-hidden="true"></span> Layers
-                        <button type="button" className="close pull-right" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </h5>
-                </div>
-                <div className="card-body">
-                    <LayerList layers={this.props.overlayLayers.layers} globe={this.props.globe}/>
-                    <hr/>
-                    <LayerList layers={this.props.baseLayers.layers} globe={this.props.globe}/>
-                </div>
+  render() {
+    // TODO: process layer lists by category.
+    let key = 1
+    let cardBody = this.props.layerLists.map((layerList) =>
+      <LayerList key={key++} layers={layerList} globe={this.props.globe} />
+    )
+    return (
+        <div className="card globe-card w-100">
+            <div className="card-header">
+                <h5 className="card-title">
+                    <span className="fas fa-list" aria-hidden="true"></span> Layers
+                    <button type="button" className="close pull-right" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </h5>
             </div>
-        );
-    }
+            <div className="card-body">
+              {cardBody}
+            </div>
+        </div>
+    );
+  }
 };
