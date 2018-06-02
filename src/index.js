1c64baec91c1f555fd03d25bcb17f2226aa0b29a
Bruce Schubert <bruce@emxsys.com>
2018-06-02 15:38:39 -0700

    Minor edits.

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@
  * The MIT License
  * http://www.opensource.org/licenses/mit-license
  */
-import LayerButton from './components/LayerButton';
-import LayerList from './components/LayerList';
-import LayersCard from './components/LayersCard';
-import MarkersCard from './components/MarkersCard';
-import NavBar from './components/NavBar';
-import NavBarItem from './components/NavBarItem';
-import SettingsCard from './components/SettingsCard';
-import SearchBox from './components/SearchBox';
-import SearchPreview from './components/SearchPreview';
-import Tools from './components/Tools';
+import LayerButton from './components/LayerButton'
+import LayerList from './components/LayerList'
+import LayersCard from './components/LayersCard'
+import MarkersCard from './components/MarkersCard'
+import NavBar from './components/NavBar'
+import NavBarItem from './components/NavBarItem'
+import SettingsCard from './components/SettingsCard'
+import SearchBox from './components/SearchBox'
+import SearchPreview from './components/SearchPreview'
+import Tools from './components/Tools'
 
 export {
-  LayersCard,
   LayerList,
   LayerButton,
+  LayersCard,
   MarkersCard,
   NavBar,
   NavBarItem,
