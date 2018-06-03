80dc61c1bff43b64df0301255052626299b4c225
Bruce Schubert <bruce@emxsys.com>
2018-06-03 10:57:37 -0700

    Refactored the SearchBox and SearchPreview to use reactstrap.

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -7,108 +7,103 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Globe from 'worldwind-react-globe';
 import FontAwesome from 'react-fontawesome';
+import { Button } from 'reactstrap';
 import SearchPreview from './SearchPreview';
-import Modal from './Modal';
 
 /* global WorldWind */
 
 export default class SearchBox extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: '', 
-            results: []
-        };
-        this.geocoder = new WorldWind.NominatimGeocoder();
-        
-        this.handleChange = this.handleChange.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleHideModal = this.handleHideModal.bind(this);
-        this.handleGotoResult = this.handleGotoResult.bind(this);
-    }
     
     static propTypes = {
-        globe: PropTypes.instanceOf(Globe),
-        mapQuestApiKey: PropTypes.string
+      globe: PropTypes.instanceOf(Globe),
+      mapQuestApiKey: PropTypes.string
     }  
     
+    constructor(props) {
+      super(props)
+      this.state = {
+          value: '', 
+          results: []
+      }
+      this.geocoder = new WorldWind.NominatimGeocoder()
+
+      this.handleChange = this.handleChange.bind(this)
+      this.handleClick = this.handleClick.bind(this)
+      this.handleSubmit = this.handleSubmit.bind(this)
+      this.hideModal = this.hideModal.bind(this)
+      this.gotoSelection = this.gotoSelection.bind(this)
+    }
+    
     handleChange(event) {
-        this.setState({value: event.target.value});
+      this.setState({value: event.target.value})
     }
 
     handleClick(event) {
-        event.preventDefault();
-        this.performSearch();
+      event.preventDefault();
+      this.performSearch();
     }
     
     handleSubmit(event) {
-        event.preventDefault();
-        this.performSearch();
-        return false;   // prevent page from refreshing
+      event.preventDefault()
+      this.performSearch()
+      return false   // prevent page from refreshing
     }
 
-    handleHideModal() {
-        // Nothing to display
-        this.setState({results: []});
+    hideModal() {
+      // Nothing to display
+      this.setState({results: []})
     }
     
-    handleGotoResult(result) {
-        const latitude = parseFloat(result.lat);
-        const longitude = parseFloat(result.lon);
+    gotoSelection(selection) {
+      if (selection) {
+        const latitude = parseFloat(selection.lat)
+        const longitude = parseFloat(selection.lon)
         // Update the globe 
-        this.props.globe.goTo(latitude, longitude);
+        this.props.globe.goTo(latitude, longitude)
+        this.hideModal();
+      }
     }
-    
+  
     performSearch () {
-        const globe = this.props.globe;
-        
-        if (!this.props.mapQuestApiKey) {
-            console.error("SearchViewModel: A MapQuest API key is required to use the geocoder in production. Get your API key at https://developer.mapquest.com/");
+      const globe = this.props.globe
+
+      if (!this.props.mapQuestApiKey) {
+          console.error("SearchViewModel: A MapQuest API key is required to use the geocoder in production. Get your API key at https://developer.mapquest.com/")
+      }
+      const queryString = this.state.value
+      if (queryString) {
+        if (queryString.match(WorldWind.WWUtil.latLonRegex)) {
+            // Treat the text as a lat, lon pair 
+            let tokens = queryString.split(",")
+            let latitude = parseFloat(tokens[0])
+            let longitude = parseFloat(tokens[1])
+            // Center the globe on the lat, lon
+            globe.wwd.goTo(new WorldWind.Location(latitude, longitude))
+        } else {
+            // Treat the text as an address or place name
+            let self = this
+            this.geocoder.lookup(queryString, function (geocoder, results) {
+                // Open the modal dialog to preview and select a result
+                // The modal is rendered when results > 0
+                self.setState({results: results})
+            }, this.props.mapQuestApiKey)
         }
-        const queryString = this.state.value;
-        if (queryString) {
-            if (queryString.match(WorldWind.WWUtil.latLonRegex)) {
-                // Treat the text as a lat, lon pair 
-                let tokens = queryString.split(",");
-                let latitude = parseFloat(tokens[0]);
-                let longitude = parseFloat(tokens[1]);
-                // Center the globe on the lat, lon
-                globe.wwd.goTo(new WorldWind.Location(latitude, longitude));
-            } else {
-                // Treat the text as an address or place name
-                let self = this;
-                this.geocoder.lookup(queryString, function (geocoder, results) {
-                    // Open the modal dialog to preview and select a result
-                    // The modal is rendered when results > 0
-                    self.setState({results: results});
-                }, this.props.mapQuestApiKey);
-            }
-        }
+      }
     }
     
     render() {
-        // The <Modal/> element renders its child outside the root DOM element allowing
-        // the <SearchPreview/> modal componment to render on top of the root elements.
-        // We'll render the modal when there are search results to display.
-        let modal = this.state.results.length > 0 ?                  
-            <Modal>
-                <SearchPreview 
-                    globe={this.props.globe}
-                    results={this.state.results}
-                    handleHideModal={this.handleHideModal}
-                    handleGotoSelection={this.handleGotoResult}
-                    showApiWarning={!this.props.mapQuestApiKey}/>
-            </Modal> : null;
-    
-        return (
-            <form className="form-inline" onSubmit={this.handleSubmit}>
-                <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" value={this.state.value} onChange={this.handleChange}/>
-                <button className="btn btn-outline-success" type="button" onClick={this.handleClick}>
-                    <FontAwesome name='search'/> 
-                </button>
-                {modal}
-            </form>
-        );
+      return (
+          <form className="form-inline" onSubmit={this.handleSubmit}>
+              <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" value={this.state.value} onChange={this.handleChange}/>
+              <Button color="success" onClick={this.handleClick}>
+                  <FontAwesome name='search'/> 
+              </Button>
+              <SearchPreview 
+                results={this.state.results}
+                handleHideModal={this.hideModal}
+                handleGotoSelection={this.gotoSelection}
+                showApiWarning={!this.props.mapQuestApiKey}/>
+          </form>
+      );
     }
 };
--- src/components/SearchPreview.js
+++ src/components/SearchPreview.js
@@ -3,141 +3,148 @@
  * The MIT License
  * http://www.opensource.org/licenses/mit-license
  */
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-import Globe from 'worldwind-react-globe';
-import $ from 'jquery';
-import 'bootstrap';
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import Globe from 'worldwind-react-globe'
+import { 
+  Alert,
+  Button, 
+  Modal, 
+  ModalHeader, 
+  ModalBody, 
+  ModalFooter } from 'reactstrap'
 
-import './SearchPreview.css';
+import './SearchPreview.css'
 
 /* global WorldWind */
 
 class SearchPreview  extends Component{
-    constructor(props) {
-        super(props);
-        this.globe = null;
-        this.state = {selection: this.props.results[0]};
-        this.globeRef = React.createRef();
+  
+  static propTypes = {
+    title: PropTypes.string,
+    results: PropTypes.array.isRequired,
+    handleHideModal: PropTypes.func.isRequired,
+    handleGotoSelection: PropTypes.func.isRequired,
+    showApiWarning: PropTypes.bool
+  }  
+  static defaultProps = {
+    title: 'Search Results',
+    showApiWarning: true
+  }  
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      showWarning: this.props.showApiWarning
+    }
+    this.dismissWarning = this.dismissWarning.bind(this)
+    this.handleGotoClick = this.handleGotoClick.bind(this)
+    this.handlePreviewClick = this.handlePreviewClick.bind(this)
+    this.globeRef = React.createRef()
+    
+    this.globe = null;
+    this.selection = null
+    this.resultsLayer = null
+    this.placemarkAttributes = new WorldWind.PlacemarkAttributes(null)
+    this.placemarkAttributes.imageSource = "https://files.worldwind.arc.nasa.gov/artifactory/web/0.9.0/images/pushpins/castshadow-red.png"
+    this.placemarkAttributes.imageScale = 0.5
+    this.placemarkAttributes.imageOffset = new WorldWind.Offset(
+          WorldWind.OFFSET_FRACTION, 0.3,
+          WorldWind.OFFSET_FRACTION, 0.0)    
+  }
+  
+  dismissWarning() {
+    this.setState({
+      showWarning: false
+    })
+  }
 
-        this.handleGotoClick = this.handleGotoClick.bind(this);
-        this.handlePreviewClick = this.handlePreviewClick.bind(this);
+
+  handlePreviewClick(result) {
+    let latitude = parseFloat(result.lat),
+        longitude = parseFloat(result.lon)      
+    this.globeRef.current.goTo(latitude, longitude) 
+    // Update the selection used for the Go To button
+    this.selection = result
+  }
+
+  handleGotoClick() {
+    // Call the parent's handler to process the selection
+    this.props.handleGotoSelection(this.selection)
+  }
+  
+  createMarker(result) {
+    let marker = new WorldWind.Placemark(
+        new WorldWind.Position(
+            parseFloat(result.lat),
+            parseFloat(result.lon), 100))
+    marker.attributes = this.placemarkAttributes
+    marker.altitudeMode = WorldWind.RELATIVE_TO_GROUND
+    marker.displayName = result.display_name  
+    return marker
+  }
+  
+  /**
+   * Updates the preview globe AFTER the modal is shown.
+   * This cannot be done in componentDidMount because the modal is not being shown
+   * at that time
+   * @param {type} prevProps
+   * @param {type} prevState
+   */
+  componentDidUpdate(prevProps, prevState) {
+    if (this.globeRef.current) {
+      const globe = this.globeRef.current
+      const resultsLayer = globe.addLayer('renderables');
+      this.props.results.forEach((result) => {        
+        resultsLayer.addRenderable(this.createMarker(result)) 
+     })  
     }
-    
-    static propTypes = {
-        globe: PropTypes.instanceOf(Globe),
-        results: PropTypes.array,
-        handleHideModal: PropTypes.func,
-        handleGotoSelection: PropTypes.func,
-        showApiWarning: PropTypes.bool
-    }  
-    
-    handlePreviewClick(result) {
-        // Update the selection used for the Go To button
-        this.setState({selection: result});
-        
-        let latitude = parseFloat(result.lat),
-            longitude = parseFloat(result.lon);
-        this.globe.goTo(latitude, longitude); 
-    }
-    
-    handleGotoClick() {
-        // Call the parent's handler to process the selection
-        this.props.handleGotoSelection(this.state.selection);
-    }
-    
-    
-    componentDidMount(){
-        this.globe = this.globeRef.current;
-        this.globe.addLayer(new WorldWind.BingRoadsLayer(), { detailControl: 1.25 });
-        
-        // Create pushpin placemarks to represent the results on the globe
-        let resultsLayer = new WorldWind.RenderableLayer();
-        let placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
-        placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/pushpins/castshadow-red.png";
-        placemarkAttributes.imageScale = 0.5;
-        placemarkAttributes.imageOffset = new WorldWind.Offset(
-            WorldWind.OFFSET_FRACTION, 0.3,
-            WorldWind.OFFSET_FRACTION, 0.0);
-            
-        this.props.results.forEach(item => {
-            let placemark = new WorldWind.Placemark(
-                new WorldWind.Position(
-                    parseFloat(item.lat),
-                    parseFloat(item.lon), 100));
-            placemark.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
-            placemark.displayName = item.display_name;
-            placemark.attributes = placemarkAttributes;
-            resultsLayer.addRenderable(placemark); 
-        });    
-        this.globe.addLayer(resultsLayer);
-        
-        // Show this dialog when mounted and call the prop function when closed
-        const element = ReactDOM.findDOMNode(this);
-        $(element).modal('show');
-        $(element).on('hidden.bs.modal', this.props.handleHideModal);
-    }
-    
-   
-    render() {
-        const self = this;
-        
-        function TableRow(props) {
-            return (
-                <tr key={props.result.place_id} onClick={()=>self.handlePreviewClick(props.result)}>
-                    <td>{props.result.display_name}</td>
-                    <td>{props.result.type}</td>
-                </tr>    
-            );
-        }
-        const tableRows = this.props.results.map((result) =>
-            <TableRow result={result}/>
-        );
-        
-        return (
-            <div className="modal fade" role="dialog">
-                <div className="modal-dialog">
-                  <div className="modal-content">
-                    <div className="modal-header">
-                        {this.props.title}
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div className="modal-body" >
-                        <div className="preview-globe">
-                            <Globe id="preview-globe" projection="Mercator" ref={this.globeRef}/>
-                        </div>
-                        <div className="modal-body-table">
-                            <div className="alert alert-warning alert-dismissible fade show" role="alert" data-bind="visible: showApiWarning">
-                                MapQuest API key missing. Get a free key at <a href="https://developer.globequest.com/" className="alert-link" rel="noopener noreferrer" target="_blank">developer.globequest.com</a> and set the MAPQUEST_API_KEY variable to your key.
-                                <button type="button" className="close" data-dismiss="alert" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>                                        
-                            </div>                                        
-                            <table className="table table-hover">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Type</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {tableRows}
-                                </tbody>
-                            </table>                                       
-                        </div>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={this.handleGotoClick}>Go To</button>
-                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                  </div>
-                </div>
-            </div>
-        );
-    }
+  }
+  
+  render() {
+       
+    this.selection = this.props.results[0]
+    const tableRows = this.props.results.map((result) =>
+        <tr key={result.place_id} onClick={()=>this.handlePreviewClick(result)}>
+            <td>{result.display_name}</td>
+            <td>{result.type}</td>
+        </tr>  
+    )
+  
+    return (
+      <Modal isOpen={this.props.results.length > 0} toggle={this.props.handleHideModal}>
+        <ModalHeader toggle={this.props.handleHideModal}>{this.props.title}</ModalHeader>
+        <ModalBody>
+          <div className="preview-globe">
+            <Globe id="preview-globe" projection="Mercator" ref={this.globeRef}/>
+          </div>
+          <div className="modal-body-table">
+              <Alert color="warning" isOpen={this.state.showWarning}>
+                <Button className="close pull-right" aria-label="Close" onClick={this.dismissWarning} >
+                  <span aria-hidden="true">&times;</span>
+                </Button>
+                MapQuest API key missing. Get a free key at <a href="https://developer.globequest.com/" className="alert-link" rel="noopener noreferrer" target="_blank">developer.globequest.com</a> and set the MAPQUEST_API_KEY variable to your key.
+              </Alert>                                     
+              <table className="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Type</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {tableRows}
+                </tbody>
+              </table>                                       
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={this.handleGotoClick}>Go To</Button>{' '}
+          <Button onClick={this.props.handleHideModal}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
 };
 
 export default SearchPreview;
